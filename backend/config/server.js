7a5344eb92d65c77f34d8d0f61894f7963ca9991
aurellya <aurellyagho25@gmail.com>
2021-05-21 16:25:22 +0800

    [*****]

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f504b5e3fac3bd5ae0bebc948986765'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e165ddcc-0845-4215-aa02-267233cbe5fe'
+};
